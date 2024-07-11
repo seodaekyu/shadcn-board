c028bf1bd21dbfa9da410dc5ab67344966413369
seodaekyu <grarlove7@naver.com>
2024-07-11 14:35:16 +0900

    Shadcn UI 활용한 퍼블리싱1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-function page() {
+import { Button } from "@/components/ui/button";
+import styles from "./page.module.scss";
+
+function Home() {
   return (
-    <div>안녕하세요</div>
+    <div className={styles.container}>
+		<div className={styles.container__onBoarding}>
+			<span className={styles.container__onBoarding_title}>How to Start:</span>
+			<div className={styles.container__onBoarding_steps}>
+				<span>1. Create a page</span>
+				<span>2. Add boards to page</span>
+			</div>
+			{/* 페이지 추가 버튼 */}
+			<Button variant={"outline"} className="w-full bg-transparent text-orange-500 border-orange-400 hover:bg-orange-50 hover:text-orange-500">
+				Add New Page
+			</Button>
+		</div>
+	</div>
   )
 }
 
-export default page
+export default Home
